fc5524e688afc8b86646bec305935cbfeecdf8b4
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-09-04 17:02:02 +0200

    Tests and component code passing tests

--- src/GuessWords.js
+++ src/GuessWords.js
@@ -5,6 +5,27 @@ const GuessWords = (props) => {
 	let content
 	if (props.guessedWords.length === 0) {
 		content = <span data-test="guess-instructions">Give it a go!</span>
+	} else {
+		const guessedWordRows = props.guessedWords.map((word, index) => (
+			<tr data-test="guessed-word" key={index}>
+				<td>{word.guessedWord}</td>
+				<td>{word.letterMatchCount}</td>
+			</tr>
+		))
+		content = (
+			<div data-test="guessed-words">
+				<h3>Guessed Words</h3>
+				<table>
+					<thead>
+						<tr>
+							<th>Guess</th>
+							<th>Matching Letters</th>
+						</tr>
+					</thead>
+					<tbody>{guessedWordRows}</tbody>
+				</table>
+			</div>
+		)
 	}
 	return <div data-test="component-guessed-words">{content}</div>
 }
