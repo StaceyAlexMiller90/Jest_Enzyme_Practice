8fc24d1688c0cf9921c16a321b67a3126b34bc56
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-09-04 16:42:05 +0200

    Initial GuessWords tests passing

--- src/GuessWords.js
+++ src/GuessWords.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const GuessWords = () => {
-	return <div></div>
+const GuessWords = (props) => {
+	let content
+	if (props.guessedWords.length === 0) {
+		content = <span data-test="guess-instructions">Give it a go!</span>
+	}
+	return <div data-test="component-guessed-words">{content}</div>
 }
 GuessWords.propTypes = {
 	guessedWords: PropTypes.arrayOf(
